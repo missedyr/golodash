a80000a3044e5b1a1ef637debaa7e3c0a9f1538a
xuexin <xuexin@guanghe.tv>
2022-03-15 14:08:20 +0800

    concat

--- test.go
+++ test.go
@@ -1,24 +1,29 @@
 package test
 
-//func main()  {
-//	arr := lodash.DashSliceInterface{1,2,3,"4",5}
-//
-//	log.Print("11111  ", lodash.IndexOfInterface(arr, "5"))
-//
-//	arr2 := []int8{1,2,3,4,5}
-//	log.Print("22222  ", lodash.IndexOfInt8(arr2, 3))
-//
-//	arr3 := []string{"1","2","3","ios"}
-//	log.Print("33333  ", lodash.IndexOfString(arr3, "ios"))
-//
-//	aa := map[string]int64{"sdf":2}
-//	bb := map[string]string{"sdfs":"shd"}
-//	cc := map[string]string{"sddfs":"34"}
-//
-//	type Dd struct {
-//		A1 string
-//	}
-//	dd := Dd{A1: "2314"}
-//	log.Print("5555", lodash.Assign(aa, bb, cc, dd))
-//}
+func test()  {
+	//arr := lodash.DashSliceInterface{1,2,3,"4",5}
+	//
+	//log.Print("11111  ", lodash.IndexOfInterface(arr, "5"))
+	//
+	//arr2 := []int8{1,2,3,4,5}
+	//log.Print("22222  ", lodash.IndexOfInt8(arr2, 3))
+	//
+	//arr3 := []string{"1","2","3","ios"}
+	//log.Print("33333  ", lodash.IndexOfString(arr3, "ios"))
+	//
+	//aa := map[string]int64{"sdf":2}
+	//bb := map[string]string{"sdfs":"shd"}
+	//cc := map[string]string{"sddfs":"34"}
+	//
+	//type Dd struct {
+	//	A1 string
+	//}
+	//dd := Dd{A1: "2314"}
+	//log.Print("5555", lodash.Assign(aa, bb, cc, dd))
+
+	//arr1:= []interface{}{"a", 1}
+	//arr2:= []interface{}{5.5, []string{"ff"}}
+	//list := lodash.ConcatOfInterface(arr1, arr2)
+	//fmt.Printf("11111----", list)
+}
 
